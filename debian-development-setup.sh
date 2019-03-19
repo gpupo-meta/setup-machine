56c82634ba5b0ca72dc1e0f7720b5d0729c02274
Gilmar Pupo <contact@gpupo.com>
2019-03-19 16:48:07 -0300

    Add composer

--- debian-development-setup.sh
+++ debian-development-setup.sh
@@ -80,13 +80,22 @@ command -v locate >/dev/null 2>&1 || {
 
 #PHP and xtras
 command -v php >/dev/null 2>&1 || {
-
   sudo apt-get install -y apt-utils iputils-ping procps\
     unzip zlib1g-dev libpng-dev libjpeg-dev gettext-base libxml2-dev libzip-dev \
     libmcrypt-dev mysql-client libicu-dev \
     libcurl4-openssl-dev pkg-config libssl-dev telnet vim netcat \
     php-intl php-gd php-apcu php-zip;
+}
 
+#Composer
+command -v composer >/dev/null 2>&1 || {
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
+  php composer-setup.php;
+  php -r "unlink('composer-setup.php');"
+  sudo mkdir -p /usr/local/bin/;
+  sudo mv -v composer.phar /usr/local/bin/composer; sudo chmod +x /usr/local/bin/composer;
+  composer global require bamarni/symfony-console-autocomplete:dev-master;
 }
 
 sudo apt --fix-broken install;
