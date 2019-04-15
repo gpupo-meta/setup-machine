d6321eb08947d272c73a69f867bc9b96bc401b1b
Gilmar Pupo <contact@gpupo.com>
2019-04-15 14:41:10 -0300

    Improve Ubuntu script

--- debian/common-setup.sh
+++ debian/common-setup.sh
@@ -1,40 +1,4 @@
 
-command -v make >/dev/null 2>&1 || {
-  cat bin/.bash_aliases > ~/.bash_aliases;
-  sudo cp -f debian/sources.list /etc/apt/sources.list;
-  cat config/.env > ~/.env;
-  pushd ~ && source .env;
-  popd;
-  mkdir -p ~/workspace/
-  sudo apt-get -q update;
-
-  sudo apt-get -y install libcurl3 gconf2 curl python apt-utils iputils-ping telnet openssh-server \
-      apt-transport-https \
-      ca-certificates \
-      wget gnupg2 tree \
-      git-all git-cola make \
-      software-properties-common \
-      inkscape clang-format-3.8 gimp firefox-esr libappindicator3-1 \
-      libindicator3-7 libdbusmenu-glib4 libdbusmenu-gtk3-4;
-}
-
-#Docker
-command -v docker >/dev/null 2>&1 || {
-  curl -fsSL https://get.docker.com -o /tmp/get-docker.sh;
-  sudo sh /tmp/get-docker.sh;
-  sudo groupadd docker;
-  sudo usermod -aG docker $USER;
-}
-
-#docker-compose
-command -v docker-compose >/dev/null 2>&1 || {
-  sudo rm /usr/bin/docker-compose
-  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
-  sudo chmod +x /usr/local/bin/docker-compose;
-  sudo ln -snf /usr/local/bin/docker-compose /usr/bin/docker-compose ;
-  docker-compose version;
-}
-
 # Httpd Gateway
 if [ ! -d ~/httpd-gateway ]; then
   pushd ~ && git clone https://github.com/gpupo/httpd-gateway.git && pushd httpd-gateway && make setup;
--- ubuntu-development-setup.sh
+++ ubuntu-development-setup.sh
@@ -12,10 +12,9 @@ sudo apt-get -y install \
 
 #Add Docker (https://docs.docker.com/install/linux/docker-ce/ubuntu/#set-up-the-repository)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu cosmic stable"
 
 sudo apt-get update;
 sudo apt-get install docker-ce docker-ce-cli containerd.io;
-#source ./debian/common-setup.sh;
+source ./debian/common-setup.sh;
